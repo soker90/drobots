15df2689abddc19ac3401d2a5ff3628ca596a465
soker90 <edue17@gmail.com>
2016-05-29 02:08:55 +0200

    Robots de ataque y defensa

--- Client.py
+++ Client.py
@@ -37,72 +37,154 @@ class PlayerI(drobots.Player):
         self.adapter = adapter #current
 
     def makeController(self,bot,current=None):
-        RobotControllerServant = RobotControllerI(bot)
+        if (bot.ice_isA("::drobots::Attacker")):
+            RobotControllerServant = RobotControllerAtaque(bot)
+        elif (bot.ice_isA("::drobots::Defender")):
+            RobotControllerServant = RobotControllerDefensa(bot)
         proxyRobotController = current.adapter.addWithUUID(RobotControllerServant)
         return drobots.RobotControllerPrx.uncheckedCast(proxyRobotController)
     def win(self,current=None):
         print("Ganaste")
+        current.adapter.getCommunicator().shutdown()
     def lose(self,current=None):
         print("Perdiste")
+        current.adapter.getCommunicator().shutdown()
 
-class RobotControllerI(drobots.RobotController):
+class RobotControllerAtaque(drobots.RobotController):
     def __init__(self,bot):
         self.bot = bot
         self.speed = 100
+        self.i = 0
     def turn(self,current=None):
         location = self.bot.location()
         angulo = math.atan(location.y/location.x)
-
-        if(location.x == 500):
-            if location.y > 500:
-                if(self.bot.scan(angulo,self.speed)):
-                    self.bot.cannon(270,self.speed)
-                else:
-                    self.bot.drive(270,self.speed)
-            else:
-                if(self.bot.scan(90,self.speed)):
-                    self.bot.cannon(90,self.speed)
-                else:
-                    self.bot.drive(90,self.speed)
-        elif location.y == 500:
-            if location.x > 500:
-                if(self.bot.scan(180,self.speed)):
-                    self.bot.cannon(180,self.speed)
-                else:
-                    self.bot.drive(180,self.speed)
-            else:
-                if(self.bot.scan(0,self.speed)):
-                    self.bot.cannon(0,self.speed)
-                else:
-                    self.bot.drive(0,self.speed)
-        elif(location.y <500 and location.x <500):
-            if(self.bot.scan(angulo,self.speed)):
-                self.bot.cannon(angulo,self.speed)
-            else:
-                self.bot.drive(angulo,self.speed)
-        elif(location.y >500 and location.x >500):
-            if(self.bot.scan(180+angulo,self.speed)):
-                self.bot.cannon(180+angulo,self.speed)
-            else:
-                self.bot.drive(180+angulo,self.speed)
-        elif(location.y >500 and location.x <500):
-            if(self.bot.scan(270+angulo,self.speed)):
-                self.bot.cannon(270+angulo,self.speed)
-            else:
-                self.bot.drive(270+angulo,self.speed)
-        elif(location.y <500 and location.x > 500):
-            if(self.bot.scan(90+angulo,self.speed)):
-                self.bot.cannon(90+angulo,self.speed)
-            else:
-                self.bot.drive(90+angulo,self.speed)
-
-        if(location.x  > 490 and location.x < 510 and location.y  > 480 and location.y < 510):
-            self.speed = 10
-            if location.x == 500 and location.y == 500:
-                self.bot.drive(angulo,40)
+        if(self.i%2==0):
+            self.bot.cannon(angulo, self.speed)
         else:
-            self.speed = 100
+            self.bot.drive(angulo, self.speed)
 
+        self.i = self.i+1
+
+        # if(location.x == 500):
+        #     if location.y > 500:
+        #         if(self.bot.scan(angulo,self.speed)):
+        #             self.bot.cannon(270,self.speed)
+        #         else:
+        #             self.bot.drive(270,self.speed)
+        #     else:
+        #         if(self.bot.scan(90,self.speed)):
+        #             self.bot.cannon(90,self.speed)
+        #         else:
+        #             self.bot.drive(90,self.speed)
+        # elif location.y == 500:
+        #     if location.x > 500:
+        #         if(self.bot.scan(180,self.speed)):
+        #             self.bot.cannon(180,self.speed)
+        #         else:
+        #             self.bot.drive(180,self.speed)
+        #     else:
+        #         if(self.bot.scan(0,self.speed)):
+        #             self.bot.cannon(0,self.speed)
+        #         else:
+        #             self.bot.drive(0,self.speed)
+        # elif(location.y <500 and location.x <500):
+        #     if(self.bot.scan(angulo,self.speed)):
+        #         self.bot.cannon(angulo,self.speed)
+        #     else:
+        #         self.bot.drive(angulo,self.speed)
+        # elif(location.y >500 and location.x >500):
+        #     if(self.bot.scan(180+angulo,self.speed)):
+        #         self.bot.cannon(180+angulo,self.speed)
+        #     else:
+        #         self.bot.drive(180+angulo,self.speed)
+        # elif(location.y >500 and location.x <500):
+        #     if(self.bot.scan(270+angulo,self.speed)):
+        #         self.bot.cannon(270+angulo,self.speed)
+        #     else:
+        #         self.bot.drive(270+angulo,self.speed)
+        # elif(location.y <500 and location.x > 500):
+        #     if(self.bot.scan(90+angulo,self.speed)):
+        #         self.bot.cannon(90+angulo,self.speed)
+        #     else:
+        #         self.bot.drive(90+angulo,self.speed)
+        #
+        # if(location.x  > 490 and location.x < 510 and location.y  > 480 and location.y < 510):
+        #     self.speed = 10
+        #     if location.x == 500 and location.y == 500:
+        #         self.bot.drive(angulo,40)
+        # else:
+        #     self.speed = 100
+
+
+    def robotDestroyed(self, current=None):
+        print("El robot ha sido destruido")
+
+class RobotControllerDefensa(drobots.RobotController):
+    def __init__(self, bot):
+        self.bot = bot
+        self.speed = 100
+        self.i = 0
+
+    def turn(self, current=None):
+        location = self.bot.location()
+        angulo = math.atan(location.y / location.x)
+
+        if (self.i % 2 == 0):
+            self.bot.scan(angulo, self.speed)
+        else:
+            self.bot.drive(angulo, self.speed)
+
+        self.i = self.i + 1
+
+        # if (location.x == 500):
+        #     if location.y > 500:
+        #         if (self.bot.scan(angulo, self.speed)):
+        #             self.bot.cannon(270, self.speed)
+        #         else:
+        #             self.bot.drive(270, self.speed)
+        #     else:
+        #         if (self.bot.scan(90, self.speed)):
+        #             self.bot.cannon(90, self.speed)
+        #         else:
+        #             self.bot.drive(90, self.speed)
+        # elif location.y == 500:
+        #     if location.x > 500:
+        #         if (self.bot.scan(180, self.speed)):
+        #             self.bot.cannon(180, self.speed)
+        #         else:
+        #             self.bot.drive(180, self.speed)
+        #     else:
+        #         if (self.bot.scan(0, self.speed)):
+        #             self.bot.cannon(0, self.speed)
+        #         else:
+        #             self.bot.drive(0, self.speed)
+        # elif (location.y < 500 and location.x < 500):
+        #     if (self.bot.scan(angulo, self.speed)):
+        #         self.bot.cannon(angulo, self.speed)
+        #     else:
+        #         self.bot.drive(angulo, self.speed)
+        # elif (location.y > 500 and location.x > 500):
+        #     if (self.bot.scan(180 + angulo, self.speed)):
+        #         self.bot.cannon(180 + angulo, self.speed)
+        #     else:
+        #         self.bot.drive(180 + angulo, self.speed)
+        # elif (location.y > 500 and location.x < 500):
+        #     if (self.bot.scan(270 + angulo, self.speed)):
+        #         self.bot.cannon(270 + angulo, self.speed)
+        #     else:
+        #         self.bot.drive(270 + angulo, self.speed)
+        # elif (location.y < 500 and location.x > 500):
+        #     if (self.bot.scan(90 + angulo, self.speed)):
+        #         self.bot.cannon(90 + angulo, self.speed)
+        #     else:
+        #         self.bot.drive(90 + angulo, self.speed)
+        #
+        # if (location.x > 490 and location.x < 510 and location.y > 480 and location.y < 510):
+        #     self.speed = 10
+        #     if location.x == 500 and location.y == 500:
+        #         self.bot.drive(angulo, 40)
+        # else:
+        #     self.speed = 100
 
     def robotDestroyed(self, current=None):
         print("El robot ha sido destruido")
